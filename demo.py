857c2bc049b5bcbf8c42b6c42b899968fcba598e
Jungsik Hwang <mulkkyul@gmail.com>
2022-02-08 22:34:27 +0900

    Add files via upload

--- demo.py
+++ demo.py
@@ -45,8 +45,10 @@ def load_data(filename):
     df = pd.read_csv(filename, header=0, delimiter=',')
     return df
 
-df_data_raw = load_data('./rawData/JT_SHP_SALES_VARTION_LIST_202112.csv').sort_values(by=["RANK_CO"], ascending=False)
+#df_data_raw = load_data('./rawData/JT_SHP_SALES_VARTION_LIST_202112.csv').sort_values(by=["RANK_CO"], ascending=False)
 
+#df_data_raw = load_data('./data_janToDec2021_top100.csv').sort_values(by=["ALL_SALES_PRICE_RATE"], ascending=False)
+df_data_raw = load_data('./data_julyToDec2021.csv').sort_values(by=["ALL_SALES_PRICE_RATE"], ascending=False)
 
 
 # =====================================================================================
@@ -64,6 +66,7 @@ st.write("")
 st.write("")
 franchise = st.checkbox("프랜차이즈 포함")
 airport = st.checkbox("제주공항 내 상점 포함")
+hotel = st.checkbox("호텔/리조트 내 식당 포함")
 
 st.markdown('___')
 
@@ -73,13 +76,17 @@ st.markdown('___')
 # =====================================================================================
 list_franchise = ["한국맥도날드","버거킹","빽다방","던킨","탐앤탐스","파리바게뜨","풀바셋","에이바우트","메가엠지씨","컴포즈",
                   "투썸플레이스","커피빈","할리스","파스쿠찌","뚜레쥬르","써브웨이","파리바게트","김밥천국",
-                  "(유)아웃백스테이크","롯데리아","KFC","맘스터치","도미노피자","교촌치킨","배스킨라빈스"]
+                  "(유)아웃백스테이크","롯데리아","KFC","맘스터치","도미노피자","교촌치킨","배스킨라빈스","아웃백스테이크하우스"]
+list_hotel = ["토스카나","한화호텔앤드리조트","제주신화월드"]
 
 df_target = df_data_raw
 if(not franchise):
     df_target = df_target[df_target["CMPNM_NM"].str.contains('|'.join(list_franchise)) == False]
 if(not airport):
     df_target = df_target[df_target["CMPNM_NM"].str.contains("공항") == False]
+if(not hotel):
+    df_target = df_target[df_target["CMPNM_NM"].str.contains('|'.join(list_hotel)) == False]
+
 if(not foodType == "모두"):
     df_target = df_target[df_target["MLSFC_NM"] == foodType]
 if(not region == "전지역"):
@@ -224,10 +231,12 @@ st.write("This map was retrieved from [INVEST KOREA](https://www.investkorea.org
 # =====================================================================================
 st.markdown('___')
 st.markdown('<p class="columnHeader"><span style=\"font-size: 90% ; color: gray\">About</span> <span style=\"color: #ea4335\">맛있는</span> 데이터 <span style=\"font-size: 60% ; color: #4285f4\">Jeju</span></br></p>', unsafe_allow_html=True)
-st.write("맛있는 데이터, 제주는 제주관광공사에서 제공하는 제주도 내 음식점별 전월 대비 매출 변화량 데이터를 이용하여 제작되었습니다. 데이터에 포함되어 있는 순위수 (RANK_CO)를 이용하여 순위를 매기고, 제주도민 매출금액 비율값과 외지인 매출금액 비율값을 이용하여, 제주도민과 외지인이 선호하는 음식점을 구분합니다.")
+st.write("맛있는 데이터, 제주는 제주관광공사에서 제공하는 제주도 내 음식점별 전월 대비 매출 변화량 데이터를 이용하여 제작되었습니다. "         
+         "2021년 하반기 (7-12월) 데이터를 취합하여, 전체 매출 금액 비율 (ALL_SALES_PRICE_RATE)로 순위를 매기고, "
+         "제주도민 매출금액 비율(JJINHBT_SALES_PRICE_RATE)과 외지인 매출금액 비율(OTSD_SALES_PRICE_RATE)을 이용하여, 제주도민과 외지인이 선호하는 음식점을 구분합니다.")
 st.write("")
 st.write("- 데이터셋: 관광 소비행태 데이터_카드사 음식 급상승 데이터 (2022).*제주관광공사*. Retrieved from [문화 빅데이터 플랫폼](https://www.bigdata-culture.kr/bigdata/user/data_market/detail.do?id=f0306b70-597a-11ec-8ee4-95f65f846b27)")
-st.write("- 소스코드: https://github.com/mulkkyul/tastyjeju")
+st.write("- 소스코드: https://github.com/mulkkyul/jeju-mat-data")
 st.write("- 맛있는 데이터, 제주를 만든 이야기가 궁금하다면 여기로 -> [브런치](https://brunch.co.kr/@mulkkyul)")
 st.write("")
 st.write("")
